fd274128113edb03d034490d00b199425277e27d
Ryan Chen <me@ryanchen.com>
2016-04-15 19:27:06 -0700

    Initial site

---
+++ mw.js
@@ -0,0 +1,31 @@
+(function() {
+  var photos = [
+    9508, 9525, 9533, 9614, 9636, 9639, 9640, 9641, 9642, 9643, 9645,
+    9646, 9648, 9669, 9683, 9687, 9705, 9711, 9721, 9726, 9751, 9764,
+    9795, 9797, 9798, 9799, 9804, 9805, 9806, 9810, 9811, 9815, 9816,
+    9817, 9818, 9819, 9855, 9856, 9931
+  ];
+  var galleriaDiv = document.getElementById('idGalleria');
+  var data = [];
+  for (var i=0; i<2; i++) {
+    //var imgElem = document.createElement('img');
+    //imgElem.src = 'photos/IMG_' + photos[i] + '.JPG';
+    //galleriaDiv.appendChild(imgElem);
+    data.push({
+      image: 'http://www.michellewang.net/photos/IMG_' + photos[i] + '.JPG',
+      title: 'Alaska',
+      description: 'Alaska'
+    })
+  }
+  Galleria.loadTheme('http://cdnjs.cloudflare.com/ajax/libs/galleria/1.4.2/themes/classic/galleria.classic.min.js');
+
+  Galleria.ready(function(options) {
+    //this.openLightbox();
+  });
+
+  Galleria.run('.galleria', {
+    width: $(window).width(),
+    height: $(window).height(),
+    dataSource: data
+  });
+})();
---
+++ questions.js
@@ -0,0 +1,21 @@
+var questions = [{
+  question: "你最喜欢的颜色?",
+  choices: ['Red', 'Green', 'Blue', 'Orange', 'Yellow'],
+  correctAnswer: 2
+}, {
+  question: "你最爱吃的水果?",
+  choices: ['Watermelon', 'Banana', 'Apple', 'Orange', 'Pineapple'],
+  correctAnswer: 0
+}, {
+  question: "下面哪一首不是你最喜欢的歌之一?",
+  choices: ['Thinking out loud', '开不了口', '那些花儿', '匆匆那年', '淘汰'],
+  correctAnswer: 4
+}, {
+  question: "最赞最棒的星座是～～～?",
+  choices: ['Gemini', 'Libra', 'Virgo', 'Scorpio', 'Cancer'],
+  correctAnswer: 1
+}, {
+  question: "在阳光灿烂的日子里",
+  choices: ['向你问好', '抱头痛哭', '游山玩水', '和你私奔', '开怀大笑'],
+  correctAnswer: 4
+}];
